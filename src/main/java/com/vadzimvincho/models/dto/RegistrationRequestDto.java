614e57e6671d3808fa206cf5ea887b347c575fdb
Vadzim Vincho <vad.vincho@mail.ru>
2021-06-08 08:01:15 +0300

    request dto

--- src/main/java/com/vadzimvincho/models/dto/RegistrationRequestDto.java
+++ src/main/java/com/vadzimvincho/models/dto/RegistrationRequestDto.java
@@ -2,9 +2,8 @@ package com.vadzimvincho.models.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
 
-@Data
+
 public class RegistrationRequestDto {
 
     @NotEmpty
@@ -16,4 +15,31 @@ public class RegistrationRequestDto {
     @Size(min = 3, message = "Не меньше 5 знаков")
     @NotEmpty
     private String passwordConfirm;
+
+    public RegistrationRequestDto() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
 }
