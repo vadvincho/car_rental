6dae21515e3a1afbb9cfd25f30f233f3aeef2eff
Vadzim Vincho <vad.vincho@mail.ru>
2021-07-30 00:33:55 +0300

    sql - criteria api

--- src/main/java/com/vadzimvincho/repositories/impl/AppUserRepositoryImpl.java
+++ src/main/java/com/vadzimvincho/repositories/impl/AppUserRepositoryImpl.java
@@ -4,8 +4,9 @@ import com.vadzimvincho.models.entity.AppUser;
 import com.vadzimvincho.repositories.api.AppUserRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.Query;
-import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 @Repository
 public class AppUserRepositoryImpl extends GenericRepositoryImpl<AppUser> implements AppUserRepository {
@@ -15,9 +16,16 @@ public class AppUserRepositoryImpl extends GenericRepositoryImpl<AppUser> implem
 
     @Override
     public AppUser findByLogin(String login) {
-        Query query = entityManager.createQuery("SELECT u FROM AppUser as u WHERE u.login = : login");
-        query.setParameter("login", login);
-        List resultList = query.getResultList();
-        return resultList.size() != 0 ? (AppUser) resultList.get(0) : null;
+
+//        Query query = entityManager.createQuery("SELECT u FROM AppUser as u WHERE u.login = : login");
+//        query.setParameter("login", login);
+//        List resultList = query.getResultList();
+//        return resultList.size() != 0 ? (AppUser) resultList.get(0) : null;
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<AppUser> query = criteriaBuilder.createQuery(AppUser.class);
+        Root<AppUser> root = query.from(AppUser.class);
+        query.select(root).where(criteriaBuilder.equal(root.get("login"), login));
+        return entityManager.createQuery(query).getSingleResult();
     }
 }
