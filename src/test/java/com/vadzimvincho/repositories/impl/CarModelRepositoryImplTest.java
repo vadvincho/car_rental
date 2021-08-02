d3f61cc640cbf5626aed6d89217db6be0053fa17
Vadzim Vincho <vad.vincho@mail.ru>
2021-08-03 00:12:46 +0300

    test

--- src/test/java/com/vadzimvincho/repositories/impl/CarModelRepositoryImplTest.java
+++ src/test/java/com/vadzimvincho/repositories/impl/CarModelRepositoryImplTest.java
@@ -1,4 +1,25 @@
-import static org.junit.Assert.*;
+package com.vadzimvincho.repositories.impl;
+
+import com.vadzimvincho.models.entity.CarModel;
+import com.vadzimvincho.repositories.api.CarModelRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class CarModelRepositoryImplTest {
-  
+
+//    CarModelRepository carModelRepository;
+//
+//    @Before
+//    public void setUp() throws Exception {
+//
+//        CarModel carModel = new CarModel();
+//        carModelRepository = Mockito.mock(CarModelRepository.class);
+//        Mockito.when(carModelRepository.getById(1l)).thenReturn(carModel);
+//    }
+//
+//    @Test
+//    void testRepo() {
+//
+//    }
 }
