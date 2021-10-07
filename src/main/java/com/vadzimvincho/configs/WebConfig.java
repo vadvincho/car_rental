db654e9fbc00ea3003b9d2c933aa5eb592661175
Vadzim Vincho <vad.vincho@mail.ru>
2021-10-07 08:47:08 +0300

    webconfig

--- src/main/java/com/vadzimvincho/configs/WebConfig.java
+++ src/main/java/com/vadzimvincho/configs/WebConfig.java
@@ -14,7 +14,7 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan({"com.vadzimvincho"})
+@ComponentScan("com.vadzimvincho")
 public class WebConfig implements WebMvcConfigurer {
     private final ApplicationContext applicationContext;
 
